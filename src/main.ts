7ceab5e214b22fcd1abf6721d67f7756afc61042
mrisney <marc.risney@gmail.com>
2019-09-08 00:59:39 -0700

    update

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,11 @@ if (environment.production) {
   enableProdMode();
 }
 
+// for enterprise customers
+import {LicenseManager} from "ag-grid-enterprise/main";
+LicenseManager.setLicenseKey("SHI_UK_on_behalf_of_Lenovo_Sweden_MultiApp_1Devs6_November_2019__MTU3Mjk5ODQwMDAwMA==e27a8fba6b8b1b40e95ee08e9e0db2cb");
+
+
+
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));

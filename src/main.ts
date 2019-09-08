08de0b5048c1aa2d4e5adaccc8377623bd71b863
mrisney <marc.risney@gmail.com>
2019-09-08 02:07:31 -0700

    initial

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {LicenseManager} from 'ag-grid-enterprise';
+
+// tslint:disable-next-line:max-line-length
+LicenseManager.setLicenseKey('Nubox_Colombia_SAS_NuboxColombia_single_1_Devs__13_August_2020_[v2]_MTU5NzI3NjgwMDAwMA==582da7f5b8433459a69d707ac9b3b719');
+
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -7,11 +12,6 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
-// for enterprise customers
-import {LicenseManager} from "ag-grid-enterprise/main";
-LicenseManager.setLicenseKey("SHI_UK_on_behalf_of_Lenovo_Sweden_MultiApp_1Devs6_November_2019__MTU3Mjk5ODQwMDAwMA==e27a8fba6b8b1b40e95ee08e9e0db2cb");
-
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)

3d98a9907174176dc30a5b2746d7ff6d3f113684
mrisney <marc.risney@gmail.com>
2019-09-08 07:31:48 -0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { HttpClientModule } from '@angular/common/http';
 import 'ag-grid-enterprise';
 import 'ag-grid-enterprise/chartsModule';
 
+import {LicenseManager} from 'ag-grid-enterprise';
+
+// tslint:disable-next-line:max-line-length
+LicenseManager.setLicenseKey('Nubox_Colombia_SAS_NuboxColombia_single_1_Devs__13_August_2020_[v2]_MTU5NzI3NjgwMDAwMA==582da7f5b8433459a69d707ac9b3b719');
+
 @NgModule({
   declarations: [
     GridComponent

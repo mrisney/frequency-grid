c06c2fae058246a83b852d5515e9b58f2a4c63ed
mrisney <marc.risney@gmail.com>
2019-09-09 02:52:35 -0500

    update

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { RestApiService } from '../services/rest-api.service';
 
 @Component({
-  selector: 'frequency-grid',
-  template: `
-  <select class="form-control">
-    <option value=""></option>
-    <option value="datasource1">Datasource1</option>
-    <option value="datasource2">Datasource2</option>
-  </select>
+    selector: 'frequency-grid',
+    template: `
+    <select style="margin-left: 5px; margin-bottom: 10px;" class="ag-theme-balham">
+        <option [value]="Datasource" *ngFor="let source of Datasource">{{source.datasource}}</option>
+    </select>
+    
+    <select style="margin-left: 10px; margin-bottom: 10px;" class="ag-theme-balham">
+        <option [value]="variable" *ngFor="let variable of Variables">{{variable.variable}}</option>
+    </select>
 
   <ag-grid-angular 
     style="width: 640px; height: 500px;" 
@@ -20,24 +23,41 @@ import { HttpClient } from '@angular/common/http';
     [columnDefs]="columnDefs">
   </ag-grid-angular>
   `,
-  styleUrls: ['grid.component.scss'],
-  encapsulation: ViewEncapsulation.ShadowDom
+    styleUrls: ['grid.component.scss'],
+    encapsulation: ViewEncapsulation.ShadowDom
 })
 export class GridComponent implements OnInit {
 
-  columnDefs = [
-    { headerName: 'County', field: 'variableCodes', sortable: true, filter: true },
-    { headerName: 'Frequency', field: 'frequency1', sortable: true, filter: true },
-    { headerName: 'Cum Frequency', field: 'frequency2', sortable: true, filter: true},
-    { headerName: 'Percent', field: 'percent1', sortable: true, filter: true },
-    { headerName: 'Cum Percent', field: 'cumulativePercent1', sortable: true, filter: true },
-  ];
-  
-  rowData: any;
-  
-  constructor(private http: HttpClient) { }
+    columnDefs = [
+        { headerName: 'County', field: 'variableCodes', sortable: true, filter: true },
+        { headerName: 'Frequency', field: 'frequency1', sortable: true, filter: true },
+        { headerName: 'Cum Frequency', field: 'frequency2', sortable: true, filter: true },
+        { headerName: 'Percent', field: 'percent1', sortable: true, filter: true },
+        { headerName: 'Cum Percent', field: 'cumulativePercent1', sortable: true, filter: true },
+    ];
 
-  ngOnInit() {
-    this.rowData = this.http.get('http://dev.itis-app.com/care-rest/api/v1/frequency-analysis?datasource=IXNS_2008-2019&filterName=County%5C%5CLaramie&suppressNulls=true&variableName=C001%3A%20County');
-  }
+    Datasource: any = [];
+    Variables: any = [];
+    rowData: any;
+
+    constructor(private http: HttpClient, public restApi: RestApiService) { }
+
+    ngOnInit() {
+        this.rowData = this.http.get('http://dev.itis-app.com/care-rest/api/v1/frequency-analysis?datasource=IXNS_2008-2019&filterName=County%5C%5CLaramie&suppressNulls=true&variableName=C001%3A%20County');
+        this.loadDataSources();
+    }
+
+    // Get links list
+    loadDataSources() {
+        return this.restApi.getDataSources().subscribe((data: {}) => {
+            this.Datasource = data;
+        });
+    }
+
+    getVariables(datasource) {
+        return this.restApi.getVariables(datasource).subscribe((data: {}) => {
+            this.Variables = data;
+        });
+    }
+
 }
---
+++ src/app/services/rest-api.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { DataSource } from '../shared/data-source';
+import { Variable } from '../shared/variable';
+
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class RestApiService {
+
+    // Define API
+    apiURL = 'https://dev.itis-app.com/care-rest';
+
+    constructor(private http: HttpClient) { }
+
+    // Http Options
+    httpOptions = {
+        headers: new HttpHeaders({
+            'Content-Type': 'application/json'
+        })
+    }
+
+    // HttpClient API get() method => Fetch DataSources list
+    getDataSources(): Observable<DataSource> {
+        return this.http.get<DataSource>(this.apiURL + '/api/v1/datasources')
+            .pipe(
+                retry(1),
+                catchError(this.handleError)
+            );
+    }
+
+    // HttpClient API get() method => Fetch Variable List
+    getVariables(datasource): Observable<Variable> {
+        return this.http.get<Variable>(this.apiURL + '/api/v1/variables?datasource=' + datasource)
+            .pipe(
+                retry(1),
+                catchError(this.handleError)
+            );
+    }
+
+    // Error handling 
+    handleError(error) {
+        let errorMessage = '';
+        if (error.error instanceof ErrorEvent) {
+            // Get client-side error
+            errorMessage = error.error.message;
+        } else {
+            // Get server-side error
+            errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+        }
+        window.alert(errorMessage);
+        return throwError(errorMessage);
+    }
+}
